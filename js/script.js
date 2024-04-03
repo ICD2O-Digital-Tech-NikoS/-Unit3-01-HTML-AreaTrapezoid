ba9adc41154447dfc4ab970864225ec11f38733d
NikoShivkumar <159164670+NikoShivkumar@users.noreply.github.com>
2024-04-03 03:11:02 +0000

    u

--- js/script.js
+++ js/script.js
@@ -1,5 +1,24 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
-//
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Niko
+// Created on: march 2022
 // This file contains the JS functions for index.html
+
+"use strict"
+
+// function to calculate pay
+function calculateArea() {
+  // declaring variable for hours worked with value set as value inputed by user 
+  let sideA = document.getElementById("sidea").value
+  
+  // declaring variable for wage with value set as value inputed by user 
+  let sideB = document.getElementById("sideb").value
+
+  // declaring variable for wage with value set as value inputed by user 
+  let height = document.getElementById("height").value
+  
+  // calculates result by multiplying the inputed wage and hours together to get total pay
+  let area = ((sideA + sideB)*height)/2;
+  document.getElementById("user-info").innerHTML = "The area is " + area.toFixed(2) + " cm²";
+
+  return area;
+ 
